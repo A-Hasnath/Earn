7d35dde8705ac07ee8a94a4267b68b8982e86e56
A-Hasnath <145743409+A-Hasnath@users.noreply.github.com>
2023-09-22 12:40:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ function changeContent() {
     <h2>New Content Heading</h2>
     <p>This is the new content after clicking the button.</p>
     <button onclick="changeContent()">Change Content</button>
-  `;
+   `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1942988090011660"
+     crossorigin="anonymous"></script>
+    ;
 }
